7f101b650e65458f84653519896c5006e0412066
Akira Kosuda <sayuen0@gmail.com>
2018-12-11 11:58:33 +0900

    クッキー保存によるログインの実装

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,19 +1,33 @@
 module SessionsHelper
+  #渡されたユーザーでログインする
   def log_in(user)
     session[:user_id] = user.id
   end
+#ユーザーのセッションを永続的にする
+  def remember(user)
+    user.remember
+    cookies.permanent.signed[:user_id] = user.id
+    cookies.permanent[:rememer_token] = user.remember_token
+  end
 
+#現在のユーザーをログアウトする
   def log_out
     session.delete(:user_id)
     @current_user = nil
   end
-
+#現在ログインしているユーザーを返す(いるなら)
   def current_user
-    if session[:user_id]
-      @current_user ||= User.find_by(id:session[:user_id])
+    if(user_id = session[:user_id])
+      @current_user ||= User.find_by(id:user_id)
+    elsif (user_id = cookies.signed[:user_id])
+      user = User.find_by(id:user_id)
+      if user && user.authenticated?(cookies[:remember_token])
+        log_in user
+        @current_uer = user
+      end
     end
   end
-
+#ユーザーがログインしていればtrueを返す
   def logged_in?
     !current_user.nil?
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181206141756) do
+ActiveRecord::Schema.define(version: 20181211023958) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -18,6 +18,8 @@ ActiveRecord::Schema.define(version: 20181206141756) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "password_digest"
+    t.string "remenber_digest"
+    t.string "remember_digest"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
