da36514672b72cb336365c5941935e8195bd61f4
Akira Kosuda <sayuen0@gmail.com>
2018-12-12 16:05:08 +0900

    管理者権限、アカウント削除を実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,27 +1,28 @@
 class UsersController < ApplicationController
-  before_action :logged_in_user , only:[:index, :edit, :update]
-  before_action :correct_user, only: [:edit, :update]
-
+  before_action :logged_in_user, only: %i[index edit update destroy]
+  before_action :correct_user, only: %i[edit update]
+  before_action :admin_user, only: :destroy
 
   def index
-    @users = User.all
+    @users = User.paginate(page: params[:page])
   end
-
 
   def show
     @user = User.find(params[:id])
   end
+
   def new
     @user = User.new
   end
+
   def create
     @user = User.new(user_params)
     if @user.save
       log_in @user
-      flash[:success]  ="Welcome to the Sample App!"
+      flash[:success] = 'Welcome to the Sample App!'
       redirect_to user_url(@user)
     else
-    render "new"
+      render 'new'
     end
   end
 
@@ -32,35 +33,45 @@ class UsersController < ApplicationController
   def update
     @user = User.find(params[:id])
     if @user.update_attributes(user_params)
-      flash[:success] = "Profie updated"
+      flash[:success] = 'Profie updated'
       redirect_to @user
     else
-      render "edit"
+      render 'edit'
     end
   end
 
+  #アカウント削除
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "User deleted"
+    redirect_to users_url
+  end
+
   private
-    def user_params
-      params.require(:user).permit(:name,:email,:password,:password_comfirmation)
+#
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_comfirmation, :admin)
+  end
+
+  # beforeアクション
+
+  # ログイン済みユーザーかどうか確認
+  def logged_in_user
+    unless logged_in?
+      store_location
+      flash[:danger] = 'Please log in'
+      redirect_to login_url
     end
+  end
 
-    #beforeアクション
-    
-    #ログイン済みユーザーかどうか確認
-    def logged_in_user
-      unless logged_in?
-        store_location
-        flash[:danger] = "Please log in"
-        redirect_to login_url
-      end
-    end
+  # 正しいユーザーかどうか確認
+  def correct_user
+    @user = User.find(params[:id])
+    redirect_to(root_url) unless current_user?(@user)
+  end
 
-    #正しいユーザーかどうか確認
-    def correct_user
-      @user = User.find(params[:id])
-      redirect_to(root_url) unless current_user?(@user)
-    end
-
-
-    
+  #管理者かどうか確認
+  def admin_user
+    redirect_to(root_url) unless current_user.admin?
+  end
 end
