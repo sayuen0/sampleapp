5c65306096772834132f56fc11a0f2831079884e
Akira Kosuda <sayuen0@gmail.com>
2018-12-12 12:32:53 +0900

    editビュー作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,9 +16,15 @@ class UsersController < ApplicationController
     end
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
   private
     def user_params
       params.require(:user).permit(:name,:email,:password,:password_comfirmation)
     end
+
+
 
 end
