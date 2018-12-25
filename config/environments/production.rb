de06e8c0f16bff580989476056178e2db0659f01
Akira Kosuda <sayuen0@gmail.com>
2018-12-25 15:41:03 +0900

    production.rbのホスト名 https://からに修正

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,7 +66,8 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.default_url_options = {host: "peaceful-mesa-55923.herokuapp.com"}
+  host = "https://peaceful-mesa-55923.herokuapp.com"
+  config.action_mailer.default_url_options = {host: host}
   ActionMailer::Base.smtp_settings = {
     :address => "smtp.sendgrid.net",
     :port => "587",
