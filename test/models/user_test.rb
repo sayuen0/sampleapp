dd49e749429d7a96a50f6707c492264c8db18833
Akira Kosuda <sayuen0@gmail.com>
2018-12-28 21:07:17 +0900

    13.2.3 プロフィール画面のマイクロポストをテストする

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -76,7 +76,7 @@ class UserTest < ActiveSupport::TestCase
 
   test "associated microposts should be destroyed" do
     @user.save
-    @user.micropost.create!(content: "Lorem ipsum")
+    @user.microposts.create!(content: "Lorem ipsum")
     assert_difference 'Micropost.count', -1 do
       @user.destroy
     end
