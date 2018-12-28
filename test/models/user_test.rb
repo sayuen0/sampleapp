584f571431a4d98a29d4037ff7f73dc1962b2471
Akira Kosuda <sayuen0@gmail.com>
2018-12-28 19:54:37 +0900

    13.1.4 マイクロポストを改良する

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -2,8 +2,9 @@ require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
   def setup
-    @user = User.new(name: "Example User", email: "user@example.com",  password: "foobar", password_confirmation: "foobar")
-  end
+    @user = User.new(name: "Example User", email: "user@example.com",
+                     password: "foobar", password_confirmation: "foobar")
+  end  
   test "should be valid" do
     assert @user.valid?
   end
@@ -72,4 +73,13 @@ class UserTest < ActiveSupport::TestCase
   test "authenticated? should return false for a usr with nil digest" do
     assert_not @user.authenticated?(:remember,"")
   end
+
+  test "associated microposts should be destroyed" do
+    @user.save
+    @user.micropost.create!(content: "Lorem ipsum")
+    assert_difference 'Micropost.count', -1 do
+      @user.destroy
+    end
+  end
+
 end
