0bc932c0df348aaf92439b387a2aef92c801e946
Akira Kosuda <sayuen0@gmail.com>
2018-12-28 23:06:06 +0900

    TODO 13.4.3 画像のリサイズ エラー修正

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -4,10 +4,10 @@ class Micropost < ApplicationRecord
   mount_uploader :picture, PictureUploader
   validates :user_id, presence: true
   validates :content, presence: true, length: {maximum: 140}
-  valodate :picture_size
+  validate :picture_size
 
   private
-  
+
   #アップロードされた画像のサイズをバリデーションする
   def picture_size
     if picture.size > 5.megabytes
