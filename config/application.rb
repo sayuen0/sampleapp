aaba35553fe383a0444b34e640c62c2ffbfddc7f
Akira Kosuda <sayuen0@gmail.com>
2018-12-29 15:21:57 +0900

    14.2.5 [Follow] ボタン (Ajax編)

--- config/application.rb
+++ config/application.rb
@@ -1,5 +1,5 @@
+require File.expand_path('../boot', __FILE__)
 require_relative 'boot'
-
 require 'rails/all'
 
 # Require the gems listed in Gemfile, including any gems
@@ -12,8 +12,7 @@ module SampleApp
     config.load_defaults 5.1
     config.time_zone = "Tokyo"
 
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
+    # 認証トークンをremoteフォームに埋め込む
+    config.action_view.embed_authenticity_token_in_remote_forms = true
   end
 end
