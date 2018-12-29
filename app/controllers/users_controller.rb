cb6c5535f29976bd0d8b2b1b8ffa4f47de96d017
Akira Kosuda <sayuen0@gmail.com>
2018-12-29 14:37:58 +0900

    14.2.3 [Following] と [Followers] ページ

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,9 @@
 class UsersController < ApplicationController
-  before_action :logged_in_user, only: %i[index edit update destroy]
+  before_action :logged_in_user, only: %i[index edit update destroy
+                                          following followers ]
   before_action :correct_user, only: %i[edit update]
   before_action :admin_user, only: :destroy
+
 
   def index
     @users = User.where(activated: true).paginate(page: params[:page])
@@ -48,6 +50,21 @@ class UsersController < ApplicationController
     redirect_to users_url
   end
 
+  def following
+    @title = "Following"
+    @user = User.find(params[:id])
+    @users = @user.following.paginate(page: params[:page])
+    render 'show_follow'
+  end
+
+  def followers
+    @title = "Followers"
+    @user = User.find(params[:id])
+    @users = @user.followers.paginate(page: params[:page])
+    render 'show_follow'
+  end
+
+
   private
 #
   def user_params
@@ -66,4 +83,6 @@ class UsersController < ApplicationController
   def admin_user
     redirect_to(root_url) unless current_user.admin?
   end
+
+
 end
